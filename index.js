3783ae35d7dfc58ac227e5b94bb5fd9e677b2bf4
Ariel Ross <arielross92@gmail.com>
2018-04-05 22:50:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll('ul.ranked-list li')
+  for (var i=0; i < ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  const lis = document.getElementById('#grand-node').querySelectorAll('ul.ranked-list li')
+  for (let i=0; i<lis.length; i++){
+    lis[i].innerHTML=(i+1).toString()
+  }
+  
+  //var grandNode = document.querySelector('#grand-node')
+  //var deepestNode = grandNode.children[0]
+  //for (var i=0; !deepestNode.children[i]; i++){
+  //  deepestNode = deepestNode.children[i]
+  //}
+  return deepestNode
+}
