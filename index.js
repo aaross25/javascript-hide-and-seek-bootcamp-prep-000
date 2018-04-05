f991747ed2adde3abd2ea1783a6480d835e3236d
Ariel Ross <arielross92@gmail.com>
2018-04-05 22:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,26 +3,30 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget(){
-  return document.querySelector('#nested .target')
+  return document.querySelector('#nested .target');
 }
 
 function increaseRankBy(n){
-  var ranks = document.querySelectorAll('ul.ranked-list li')
+  var ranks = document.querySelectorAll('ul.ranked-list li');
   for (var i=0; i < ranks.length; i++){
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
   }
 }
 
 function deepestChild(){
-  const lis = document.getElementById('#grand-node').querySelectorAll('ul.ranked-list li')
-  for (let i=0; i<lis.length; i++){
-    lis[i].innerHTML=(i+1).toString()
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+    for (let i=0, 1=list.length; i<1; i++) {
+      list[i].innerHTML = (i+1).toString();
   }
+  //const lis = document.getElementById('#grand-node').querySelectorAll('ul.ranked-list li')
+//  for (let i=0; i<lis.length; i++){
+  //  lis[i].innerHTML=(i+1).toString()
+//  }
   
   //var grandNode = document.querySelector('#grand-node')
   //var deepestNode = grandNode.children[0]
   //for (var i=0; !deepestNode.children[i]; i++){
   //  deepestNode = deepestNode.children[i]
   //}
-  return deepestNode
+//  return deepestNode
 }
